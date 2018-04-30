561bf469a0a08eaeeed1a542caeeca83fdf26f72
mebius <mebius1080p@gmail.com>
2018-04-30 19:26:08 +0900

    added cmsbase.d.ts

---
+++ src/typings/cmsbase.d.ts
@@ -0,0 +1,13 @@
+interface idObj_base {
+	"search": string,
+	"form": string,
+	"reset": string,
+	"table": string,
+	"paging": string,
+}
+interface urlObj_base {
+	"search": string,
+	"commit": string,
+	"delete": string,
+	"cancel": string,
+}
