7563c57f89ca64b733244501cbabe4e6ed399925
mebius <mebius1080p@gmail.com>
2018-10-26 00:20:04 +0900

    ts compile 高速化設定を施した

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
-const path = require('path');
+const path = require("path");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 module.exports = {
 	entry: {
-		"sample/async": './src/ts/sample/async.ts',
+		"sample/async": "./src/ts/sample/async.ts",
 		"pagingSample/index": "./src/ts/pagingSample/index.ts",
-		"buttonSample/index": "./src/ts/buttonSample/index.ts",
+		"buttonSample/index": "./src/ts/buttonSample/index.ts"
 	},
 	output: {
-		path: path.resolve(__dirname, ''),
-		filename: '[name].js'
+		path: path.resolve(__dirname, ""),
+		filename: "[name].js"
 	},
 	resolve: {
 		extensions: [".ts"]
@@ -15,10 +16,26 @@ module.exports = {
 	module: {
 		rules: [
 			{
-				test: /\.ts$/,
-				loader: "ts-loader"
+				test: /\.tsx?$/,
+				use: [
+					{ loader: "cache-loader" },
+					{
+						loader: "thread-loader",
+						options: {
+							// there should be 1 cpu for the fork-ts-checker-webpack-plugin
+							workers: require("os").cpus().length - 1
+						}
+					},
+					{
+						loader: "ts-loader",
+						options: {
+							happyPackMode: true // IMPORTANT! use happyPackMode mode to speed-up compilation and reduce errors reported to webpack
+						}
+					}
+				]
 			}
 		]
 	},
+	plugins: [new ForkTsCheckerWebpackPlugin({ checkSyntacticErrors: true })],
 	mode: "production"
 };
