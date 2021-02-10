f5d9e5ee63e9780ddc8c47077e44ca465c258bda
mebius <mebius1080p@gmail.com>
2021-02-10 22:40:30 +0900

    esbuild-loader を導入してビルド

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
+const { ESBuildPlugin, ESBuildMinifyPlugin } = require("esbuild-loader");
 
 module.exports = {
 	entry: {
@@ -20,18 +20,11 @@ module.exports = {
 			{
 				test: /\.tsx?$/,
 				use: [
-					{ loader: "cache-loader" },
 					{
-						loader: "thread-loader",
+						loader: "esbuild-loader",
 						options: {
-							// there should be 1 cpu for the fork-ts-checker-webpack-plugin
-							workers: require("os").cpus().length - 1,
-						},
-					},
-					{
-						loader: "ts-loader",
-						options: {
-							happyPackMode: true, // IMPORTANT! use happyPackMode mode to speed-up compilation and reduce errors reported to webpack
+							loader: "tsx",
+							target: "es2016",
 						},
 					},
 				],
@@ -46,9 +39,11 @@ module.exports = {
 				},
 			},
 		}),
+		new ESBuildPlugin(),
 	],
 	optimization: {
-		minimizer: [new TerserPlugin({ parallel: true })],
+		minimize: true,
+		minimizer: [new ESBuildMinifyPlugin({ target: "es2016" })],
 	},
 	mode: "production",
 };
